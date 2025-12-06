03215970522f31470bbb959320f2d9534ceac540
Artur Cruz <arturacruz@icloud.com>
2025-12-06 00:06:37 -0300

    day 2, part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,39 @@
 use std::fs;
 
+fn has_pattern(strnum: String, size: usize) -> bool {
+    for range in 1..size {
+        if !size.is_multiple_of(range) {
+            continue
+        }
+
+        let mut slices = vec![];
+
+        for i in 0..(size / range) {
+            slices.push(
+                strnum[i * range..(i+1) * range].to_string()
+            );
+        }
+
+        let mut equal = true;
+
+        for i in 0..slices.len() - 1 {
+            if slices[i] != slices[i + 1] {
+                equal = false;
+                break;
+            }
+        }
+
+        if equal {
+            return true;
+        }
+    }
+
+    false
+}
+
 fn main() {
     let text = fs::read_to_string("assets/input.txt").expect("Failed to open input file");
+    let text = text.trim();
     let ranges = text.split(',');
     let mut equals = vec![];
 
@@ -13,11 +45,7 @@ fn main() {
         for num in min..=max {
             let strnum = num.to_string();
             let size = strnum.len();
-            if size % 2 != 0 {
-                continue
-            }
-            let (a, b) = strnum.split_at(size / 2);
-            if a == b {
+            if has_pattern(strnum, size) {
                 equals.push(num);
             }
         }
