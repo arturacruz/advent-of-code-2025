35df8d655f031ef9a8fa2de60061aec71f6dfef8
Artur Cruz <arturacruz@icloud.com>
2025-12-06 16:47:05 -0300

    day 5, part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,44 +1,48 @@
-use std::{collections::HashSet, fs, ops::RangeInclusive};
-
-fn register_ids(line: &str, ranges: &mut HashSet<RangeInclusive<u64>>) {
-    let (min, max) = line.split_once('-').expect("No - in line.");
-    let (min, max) = (
-        min.parse::<u64>().expect("Failed to parse min number."),
-        max.parse::<u64>().expect("Failed to parse max number."),
-    );
-    
-    ranges.insert(min..=max); 
-}
-
-fn is_product_fresh(line: &str, ranges: &mut HashSet<RangeInclusive<u64>>) -> bool {
-    let num = line.parse::<u64>().expect("Failed to parse product ID to number.");
-    for range in ranges.iter() {
-        if range.contains(&num) {
-            return true;
-        }
-    }
-    false
-}
+use std::fs;
 
 fn main() {
     let text = fs::read_to_string("assets/input.txt").expect("Failed to open file.");
     let lines = text.lines();
-    let mut ranges = HashSet::new();
+    let mut ranges = vec![];
 
     // Fresh ids
-    let mut creating_ids = true;
-    let mut fresh = 0;
     for line in lines {
         if line.is_empty() {
-            creating_ids = false;
-            continue;
+            break;
         }
-        if creating_ids {
-            register_ids(line, &mut ranges);
-        } else if is_product_fresh(line, &mut ranges) {
-            fresh += 1;
+        let (min, max) = line.split_once('-').expect("No - in line.");
+        let (min, max) = (
+            min.parse::<u64>().expect("Failed to parse min number."),
+            max.parse::<u64>().expect("Failed to parse max number."),
+        );
+
+        ranges.push((min, max));
+    }
+
+    ranges.sort();
+
+    let mut i = 0;
+    while i < ranges.len() - 1 {
+        let (min, max) = ranges[i];
+        let (new_min, new_max) = ranges[i + 1];
+
+        if new_min > max {
+            i += 1
+        } else {
+            let max = if new_max > max {
+                new_max
+            } else {
+                max
+            };
+            ranges[i] = (min, max);  
+            ranges.remove(i + 1);
         }
     }
 
-    println!("The amount of fresh products is {fresh}.");
+    let mut fresh = 0;
+    for (min, max) in ranges {
+        fresh += max - min + 1;
+    }
+
+    println!("The product ranges include {} fresh IDs.", fresh);
 }
