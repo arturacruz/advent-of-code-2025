95af560058086c190a4ac724e256e05f76c64f3a
Artur Cruz <arturacruz@icloud.com>
2025-12-06 01:26:08 -0300

    day 4, part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -25,29 +25,36 @@ fn main() {
     }
 
     let mut rolls = 0;
+    let mut changed = true;
 
-    for (y, line) in matrix.iter().enumerate() {
-        for (x, paper) in line.iter().enumerate() {
-            if !*paper {
-                continue
-            }
-            let mut adjacent = 0;
-            for (i, j) in DIRECTIONS {
-                let (i, j) = (y as i32 + i, x as i32 + j);
-                if i < 0 || i as usize >= matrix.len() || j < 0 || j as usize >= line.len() {
+    while changed {
+        changed = false;
+        for y in 0..matrix.len() {
+            for x in 0..matrix[y].len() {
+                let paper = matrix[y][x];
+                if !paper {
                     continue
                 }
-                let (i, j) = (i as usize, j as usize);
-                if matrix[i][j] {
-                    adjacent += 1;
-                } 
-                if adjacent == 4 {
-                    break;
+                let mut adjacent = 0;
+                for (i, j) in DIRECTIONS {
+                    let (i, j) = (y as i32 + i, x as i32 + j);
+                    if i < 0 || i as usize >= matrix.len() || j < 0 || j as usize >= matrix[y].len() {
+                        continue
+                    }
+                    let (i, j) = (i as usize, j as usize);
+                    if matrix[i][j] {
+                        adjacent += 1;
+                    } 
+                    if adjacent == 4 {
+                        break;
+                    }
                 }
-            }
 
-            if adjacent < 4 {
-                rolls += 1;
+                if adjacent < 4 {
+                    matrix[y][x] = false;
+                    changed = true;
+                    rolls += 1;
+                }
             }
         }
     }
