cb46e5d3bde689e378078a83eb0049835ed4b915
Artur Cruz <arturacruz@icloud.com>
2025-12-06 22:15:22 -0300

    day 6, part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use std::{fs, iter::zip};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Operation {
     Add, Mult
 }
@@ -11,31 +11,50 @@ fn main() {
     let last_line = lines.last().expect("Expected a last line.");
 
     let mut problems = vec![];
+    let mut spaces = vec![];
     let mut operations = vec![];
 
-    for op in last_line.split_whitespace() {
-        let o = match op {
-            "+" => Operation::Add,
-            "*" => Operation::Mult,
+    let mut space = 0;
+
+    for c in last_line.chars() {
+        let op = match c {
+            '+' => Operation::Add,
+            '*' => Operation::Mult,
+            ' ' => { space += 1; continue },
             o => panic!("Invalid operation {o}")
         };
-        operations.push(o);
+        if space != 0 {
+            spaces.push(space);
+            space = 0;
+        }
+        operations.push(op);
     }
+    spaces.push(space + 1);
 
-    for line in lines.iter().take(lines.len() - 1) {
-        let tokens = line.split_whitespace();
-        for (i, token) in tokens.enumerate() {
-            let num = token.parse::<u64>().expect("Failed to parse token");
-            if i >= problems.len() {
+    let mut offset = 0;
+
+    for (idx, (space, op)) in zip(spaces, operations).enumerate() {
+        for i in offset..offset+space {
+            let mut numberstr = String::new();
+            for line in lines.iter().take(lines.len() - 1) {
+                numberstr.push_str(&line[i..=i]);
+            }
+            let numberstr = numberstr.trim();
+            if numberstr.is_empty() {
+                break
+            }
+            let num = numberstr.parse::<u64>().expect("Failed to parse number.");
+            if idx == problems.len() {
                 problems.push(num);
                 continue;
             }
 
-            match operations[i] {
-                Operation::Add => problems[i] += num,
-                Operation::Mult => problems[i] *= num,
-            };
+            match op {
+                Operation::Add => problems[idx] += num,
+                Operation::Mult => problems[idx] *= num,
+            }
         }
+        offset += space + 1;
     }
 
     println!("The sum of all problems is {}", problems.iter().sum::<u64>());
