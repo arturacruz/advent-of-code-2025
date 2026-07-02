7567ce9bd1285acd8ebed623b25fd639138b2b13
Artur Cruz <arturacruz@icloud.com>
2026-07-02 02:09:08 -0300

    2023 day 1 part 2

--- advent-of-code-2023/day1/src/main.rs
+++ advent-of-code-2023/day1/src/main.rs
@@ -1,23 +1,47 @@
 use advent_of_code_lib::*;
 
+const NUMBERS_AS_STRINGS: [&str; 10] = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+
 fn main() {
     let input = get_input(InputType::Input);
 
     let mut codes = vec![];
 
     for line in input.lines() {
-        let nums = line
-            .chars()
-            .filter(|c| c.is_numeric())
-            .collect::<Vec<_>>();
 
-        let mut number = nums.first().unwrap().to_string();
-        number.push(*nums.last().unwrap());
+        let mut nums = vec![];
 
-        let number = number.parse::<u64>().unwrap();
+        for (i, letter) in line.char_indices() {
+            // Check for number literally
+            if letter.is_numeric() {
+                nums.push(letter.to_digit(10).unwrap());
+                continue;
+            }
+
+            // Check for number as str
+            for (num, txt) in NUMBERS_AS_STRINGS.iter().enumerate() {
+                let len = txt.len();
+                if let Some(line) = line.get(i..i + len) && line == *txt {
+                    nums.push(num as u32);
+                }
+            }
+        }
+
+        let number = nums.first().unwrap() * 10 + nums.last().unwrap();
         codes.push(number);
     }
 
-    let sum = codes.iter().sum::<u64>();
+    let sum = codes.iter().sum::<u32>();
     println!("The sum of all calibration values is {sum}!")
 }
