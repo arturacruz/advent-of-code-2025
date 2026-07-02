02af9f9246d80ce9dee9786e3432f5ab0cc157ac
Artur Cruz <arturacruz@icloud.com>
2026-07-02 04:50:46 -0300

    2023 day 3 part 2

--- advent-of-code-2023/day3/src/main.rs
+++ advent-of-code-2023/day3/src/main.rs
@@ -1,99 +1,127 @@
+use std::collections::HashMap;
+
 use advent_of_code_lib::*;
 
+#[derive(Debug)]
 struct Number {
-    value: u32,
-    is_part_number: bool,
-    started: bool
+    value: usize,
+    positions: Vec<(usize, usize)>
 }
 
 impl Number {
     fn new() -> Self {
-        Self { value: 0, is_part_number: false, started: false }
+        Self { value: 0, positions: Vec::new() }
     }
 
-    fn add_value(&mut self, value: u32) {
+    fn add_value(&mut self, value: usize, pos: (usize, usize)) {
         self.value *= 10;
         self.value += value;
+        self.positions.push(pos);
     }
 
     fn start(&mut self) {
         self.value = 0;
-        self.is_part_number = false;
-        self.started = true;
     }
 
-    fn stop(&mut self) -> u32 {
-        self.started = false;
-        self.value
+    fn has_started(&self) -> bool {
+        !self.positions.is_empty()
+    }
+
+    fn stop(&mut self) -> (usize, Vec<(usize, usize)>) {
+        let pos = self.positions.clone();
+        self.positions.clear();
+        (self.value, pos)
+    }
+}
+
+#[derive(Debug)]
+struct Symbol {
+    pos: (usize, usize)
+}
+
+impl Symbol {
+    fn new(pos: (usize, usize)) -> Self {
+        Self { pos }
+    }
+}
+
+fn store_numbers(num: &mut Number, numbers: &mut HashMap<(usize, usize), usize>) {
+    if num.has_started() {
+        let (n, positions) = num.stop();
+        for pos in positions {
+            numbers.insert(pos, n);
+        }
+    }
+}
+
+fn add_directions(pos: (usize, usize), dir: (i32, i32)) -> Option<(usize, usize)> {
+    let x = pos.0 as i32 + dir.0;
+    let y = pos.1 as i32 + dir.1;
+
+    if x < 0 || y < 0 {
+        None
+    } else {
+        Some((x as usize, y as usize))
     }
 }
 
 fn main() {
     let input = get_input(InputType::Input);
 
-    let map = input
-        .lines()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+    let mut numbers = HashMap::new();
+    let mut symbols = Vec::new();
 
-
-    let mut part_nums = vec![];
-
-    let mut num = Number::new();
-    for (y, line) in map.iter().enumerate() {
-        for (x, c) in line.iter().enumerate() {
+    for (y, line) in input.lines().enumerate() {
+        let mut num = Number::new();
+        for (x, c) in line.char_indices() {
             if c.is_numeric() {
                 // If c is a number, add to current existing number, or start a new one
                 let c = c.to_digit(10).unwrap();
-                if !num.started {
+                if !num.has_started() {
                     num.start();
                 }
-                num.add_value(c);
+                num.add_value(c as usize, (x, y));
             } else {
-                // If c is not a number, stop and add to vector the current number (if there is one)
-                if num.started {
-                    let n = num.stop();
-                    if num.is_part_number {
-                        part_nums.push(n);
-                    }
-                }
-            }
+                store_numbers(&mut num, &mut numbers);
 
-            if !num.started || num.is_part_number {
-                continue;
-            }
-
-            // Check in all directions for a symbol. If there is one, set as a part number.
-            'outer: for yoff in -1..=1 {
-                for xoff in -1..=1 {
-                    if yoff == 0 && xoff == 0 {
-                        continue;
-                    }
-
-                    let (x, y) = (x as i32 + xoff, y as i32 + yoff);
-                    if y < 0 || y >= map.len() as i32 || x < 0 || x >= map[y as usize].len() as i32{
-                        continue;
-                    }
-                    
-                    let c = map[y as usize][x as usize];
-                    
-                    if !c.is_numeric() && c != '.' {
-                        num.is_part_number = true;
-                        break 'outer;
-                    }
+                // If c is not a number or a ., it is a symbol
+                if c != '.' {
+                    symbols.push(Symbol::new((x, y)))
                 }
             }
         }
 
-        if num.started {
-            let n = num.stop();
-            if num.is_part_number {
-                part_nums.push(n);
+        store_numbers(&mut num, &mut numbers);
+    }
+
+    let mut gear_ratios = vec![];
+
+    'symbol: for symbol in symbols {
+        let mut part_nums = vec![];
+        for y in -1..=1 {
+            for x in -1..=1 {
+                if x == 0 && y == 0 {
+                    continue;
+                }
+                let pos = match add_directions(symbol.pos, (x, y)) {
+                    None => continue,
+                    Some(d) => d,
+                };
+
+                if let Some(num) = numbers.get(&pos) && !part_nums.contains(&num) {
+                    if part_nums.len() >= 2 {
+                        continue 'symbol;
+                    }
+                    part_nums.push(num);
+                }
             }
+        }
+
+        if part_nums.len() == 2 {
+            gear_ratios.push(part_nums[0] * part_nums[1]);
         }
     }
 
-    let sum = part_nums.iter().sum::<u32>();
-
-    println!("The sum of all part numbers is {sum}");
+    let sum = gear_ratios.iter().sum::<usize>();
+    println!("The sum of all gear ratios is {sum}");
 }
