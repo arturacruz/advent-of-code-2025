b6d4834a0bb0348073019333fbdb830b7f6185ee
Artur Cruz <arturacruz@icloud.com>
2026-07-04 23:19:33 -0300

    2023 day 6 part 2

--- advent-of-code-2023/day6/src/main.rs
+++ advent-of-code-2023/day6/src/main.rs
@@ -4,36 +4,26 @@ fn main() {
     let input = get_input(InputType::Input);
     
     let (times, distances) = input.split_once('\n').unwrap();
-    let times = times.get(5..).unwrap()
-        .split(' ')
-        .map(|s| s.trim())
-        .filter(|s| !s.is_empty())
-        .map(|s| s.parse::<u32>().unwrap());
+    let t = times.get(5..).unwrap()
+        .split_whitespace().collect::<String>()
+        .parse::<u128>().unwrap();
 
-    let distances = distances.get(9..).unwrap()
-        .split(' ')
-        .map(|s| s.trim())
-        .filter(|s| !s.is_empty())
-        .map(|s| s.parse::<u32>().unwrap());
-    
-    let races = times.zip(distances);
+    let r = distances.get(9..).unwrap()
+        .split_whitespace().collect::<String>()
+        .parse::<u128>().unwrap();
 
-    let ways_to_win = races
-        .map(|(t, r)| {
-            let delta = (t.pow(2) - 4 * r) as f32;
-            if delta <= 0.0 { 
-                return 0; 
-            }
-
+    let prod = {
+        let delta = (t.pow(2) - 4 * r) as f64;
+        if delta <= 0.0 { 
+            0
+        } else {
             let delta = delta.sqrt();
-            let l = (t as f32 - delta) / 2.0;
-            let r = (t as f32 + delta) / 2.0;
+            let l = (t as f64 - delta) / 2.0;
+            let r = (t as f64 + delta) / 2.0;
 
-            (r.ceil() - l.floor() - 1.0) as u32
-        })
-        .collect::<Vec<_>>();
-
-    let prod = ways_to_win.iter().product::<u32>();
+            (r.ceil() - l.floor() - 1.0) as u128
+        }
+    };
 
     println!("{prod:?}");
 }
